f53808a0d335cdc2607dbf32cfad48abe78df36a
RunTiMuTeam <102729989+RunTiMuTeam@users.noreply.github.com>
2024-04-02 19:13:49 +0800

    Add files via upload

--- page3.py
+++ page3.py
@@ -6,9 +6,17 @@ import time as t
 
 q = u.Button(text='Quit',pos=[300,50])
 ab = u.Button(text='About',pos=[200,200])
+uac = u.Button(text='User Account Control Settings',pos=[200,300])
+ua = u.Button(text='Users and Groups',pos=[200,400])
 def about():
     a.show_page('page4')
 ab.on_click = about
 def quit1():
     a.show_page('page2')
 q.on_click = quit1
+def uacsb():
+    a.show_page('page8')
+uac.on_click = uacsb
+def uasb():
+    a.show_page('page9')
+uasb.on_click = uasb
